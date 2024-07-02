ebc8e5a756c11d6d56d3c93218a76b7e6674fdf1
ines <ines@outlook.com>
2024-07-02 22:07:42 +0100

    Data Warehouse

---
+++ poms/jobs/process/AMI_Assurances_As400/table_agent_as400_0.1/src/main/templates/Job_run_template.bat
@@ -0,0 +1,3 @@
+%~d0
+cd %~dp0
+java -Dtalend.component.manager.m2.repository="%cd%/../lib" -Xms256M -Xmx1024M -Dfile.encoding=UTF-8 -cp .;../lib/routines.jar;../lib/log4j-slf4j-impl-2.12.1.jar;../lib/log4j-api-2.12.1.jar;../lib/log4j-core-2.12.1.jar;../lib/log4j-1.2-api-2.12.1.jar;../lib/commons-collections-3.2.2.jar;../lib/jboss-serialization.jar;../lib/advancedPersistentLookupLib-1.2.jar;../lib/slf4j-api-1.7.25.jar;../lib/dom4j-2.1.1.jar;../lib/trove.jar;../lib/crypto-utils.jar;../lib/jt400-9.8.jar;table_agent_as400_0_1.jar; ami_assurances_1.table_agent_as400_0_1.Table_Agent_As400  %*
